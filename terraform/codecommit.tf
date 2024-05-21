b4eb3c63c41ad41360ac6a51f27ff87dfbe42c1f
Tiago Rodrigues <mail@tig.pt>
2024-05-21 09:04:03 +0100

    terraform for codecommit

---
+++ terraform/codecommit.tf
@@ -0,0 +1,6 @@
+## If you want to use codecommit instead of github, use the code bellow to create a codecommit repo
+
+# resource "aws_codecommit_repository" "test" {
+#   repository_name = "lisbon.awsusg.site"
+#   description     = "Repo for the website of AWS UG Lisbon"
+# }
